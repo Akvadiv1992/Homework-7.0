badc765347347eb76b7c377f7bb72317e0f935dc
IGOR <sahmen@mail.ru>
2022-12-16 00:51:27 +0300

    DZ 1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Random(double[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+        matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] table = new double[m,n];
+Random(table);
